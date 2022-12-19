42bd64099ab7893448c2da4defd8a15176d8144a
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2022-12-19 19:23:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,36 @@
-// Install event
-self.addEventListener('install', evt => {
-    console.log('service worker installed');
-  });
+;const staticCacheName = 'site-static';
+const assets = [
+  '/index.htm',
+  '/js/app.js',
+  '/css/style.css',
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600&display=swap',
+  'images/pic.jpg',
+  'images/scroll-top-img.png',
 
-  // activate event
-self.addEventListener('activate', evt => {
-  console.log('service worker activated');
+];
+
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+});
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
