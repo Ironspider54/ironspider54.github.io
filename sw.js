6a10e70c3ec5d454781aa2c18c861eccc76967a9
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2022-12-19 21:36:02 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+
 ;const staticCacheName = 'site-static';
 const assets = [
   '/index.htm',
@@ -6,8 +7,21 @@ const assets = [
   'https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600&display=swap',
   'images/pic.jpg',
   'images/scroll-top-img.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css',
 
 ];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 // install event
 self.addEventListener('install', evt => {
@@ -23,6 +37,15 @@ self.addEventListener('install', evt => {
 // activate event
 self.addEventListener('activate', evt => {
   //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 // fetch event
@@ -30,7 +53,15 @@ self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
-});
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dynamicCacheName, 15);
+          return fetchRes;
+        })
+      });}
+    )
+)})
+
+
