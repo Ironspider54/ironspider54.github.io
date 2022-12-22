48b6172b48fde7cbef5764106aad379c96f1e7b4
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2022-12-22 17:30:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,7 @@
 
-;const staticCacheName = 'site-static';
-const assets = [
-  'index.htm',
-  'js/app.js',
-  'css/style.css',
-  'https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600&display=swap',
-  'images/pic.jpg',
-  'images/scroll-top-img.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css',
-  'js/script.js',
-  'images/scroll-top-img.png',
-
-];
-
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if(keys.length > size){
-        cache.delete(keys[0]).then(limitCacheSize(name, size));
-      }
-    });
-  });
-};
-
 // install event
 self.addEventListener('install', evt => {
-  //console.log('service worker installed');
+  console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log('caching shell assets');
@@ -38,7 +12,7 @@ self.addEventListener('install', evt => {
 
 // activate event
 self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
+  console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
       //console.log(keys);
@@ -52,18 +26,5 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          // check cached items size
-          limitCacheSize(dynamicCacheName, 15);
-          return fetchRes;
-        })
-      });}
-    )
-)})
-
-
+  console.log('fetch event', evt);
+})
