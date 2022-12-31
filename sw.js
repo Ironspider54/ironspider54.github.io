e5bbfeb4548fe246b941a6f9b54d08ac1b2207b9
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2022-12-31 19:19:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,8 @@
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-// install event
-self.addEventListener('install', evt => {
-  console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
-
-// activate event
-self.addEventListener('activate', evt => {
-  console.log('service worker activated');
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      //console.log(keys);
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// fetch event
-self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
-})
-// This is the "Offline page" service worker
+const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
@@ -50,30 +20,16 @@ self.addEventListener('install', async (event) => {
   );
 });
 
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-const offlineFallbackPage = "Offline.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
