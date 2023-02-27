b0878ecc42bde4cf78b6ea918859a485c4442892
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2023-02-27 13:14:43 +0100

    Push

--- js/script.js
+++ js/script.js
@@ -18,3 +18,23 @@ $(document).ready(function () {
         );
     });
 });
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+};
