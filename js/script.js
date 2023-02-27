732d08893c601d849ac81501e5ff5d1708132eba
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2023-02-27 13:14:32 +0100

    Push

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-$(document).ready(function(){$("#menu").click(function(){$(this).toggleClass("fa-times"),$("header").toggleClass("toggle")}),$(window).on("scroll load",function(){$("#menu").removeClass("fa-times"),$("header").removeClass("toggle"),$(window).scrollTop()>0?$(".top").show():$(".top").hide()}),$('a[href*="#"]').on("click",function(e){e.preventDefault(),$("html, body").animate({scrollTop:$($(this).attr("href")).offset().top},500,"linear")})});
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times"), $("header").toggleClass("toggle");
+  }),
+    $(window).on("scroll load", function () {
+      $("#menu").removeClass("fa-times"),
+        $("header").removeClass("toggle"),
+        $(window).scrollTop() > 0 ? $(".top").show() : $(".top").hide();
+    }),
+    $('a[href*="#"]').on("click", function (e) {
+      e.preventDefault(),
+        $("html, body").animate(
+          {
+            scrollTop: $($(this).attr("href")).offset().top,
+          },
+          500,
+          "linear"
+        );
+    });
+});
